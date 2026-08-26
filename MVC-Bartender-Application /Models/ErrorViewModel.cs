7c1c9866490b0651e199d62dbd01ad34b9d62cf1
MszGen <dingle.genesis@gmail.com>
2026-08-26 01:12:54 -0400

    Initial MVC project

---
+++ MVC-Bartender-Application /Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVC_Bartender_Application.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
